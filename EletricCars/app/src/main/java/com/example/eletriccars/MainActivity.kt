3d0a4d9b80b47aeaaf26506d7bde4bfe2c8b9c49
donumiad <otenneto123@gmail.com>
2024-07-24 22:34:39 -0300

    Adição de campo de entrada do usuario e tratamento da informação

--- EletricCars/app/src/main/java/com/example/eletriccars/MainActivity.kt
+++ EletricCars/app/src/main/java/com/example/eletriccars/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.eletriccars
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupViewe()
+        setupListeners()
+
+    }
+
+    fun setupViewe(){
+        preco = findViewById(R.id.et_precoKwh)
+        kmPercorrido = findViewById(R.id.et_kmPercorrido)
+        btnCalcular = findViewById(R.id.btn_calcular)
+        resultado = findViewById(R.id.et_altonomia)
+    }
+
+    fun setupListeners(){
+        btnCalcular.setOnClickListener{
+            calcular()
         }
+    }
+
+    fun calcular(){
+        val entradaPreco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+
+        val altonomia = entradaPreco / km
+        resultado.text = altonomia.toString()
     }
 }
