a05700c738393fa2f6a316aad5021e0bc4e57267
donumiad <otenneto123@gmail.com>
2024-08-07 14:54:33 -0300

    adicao de permanencia de dados

--- EletricCars/app/src/main/java/com/example/eletriccars/UI/CalcularAltonomiaActivity.kt
+++ EletricCars/app/src/main/java/com/example/eletriccars/UI/CalcularAltonomiaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.eletriccars.UI
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -22,6 +23,13 @@ class CalcularAltonomiaActivity: AppCompatActivity() {
 
         setupViewe()
         setupListeners()
+        castingResult()
+    }
+
+    private fun castingResult() {
+        val valorCalculado = getSharedPref()
+        resultado.text = valorCalculado.toString()
+
     }
 
     fun setupViewe(){
@@ -47,5 +55,20 @@ class CalcularAltonomiaActivity: AppCompatActivity() {
 
         val altonomia = entradaPreco / km
         resultado.text = altonomia.toString()
+        saveSharedPref(altonomia)
+    }
+
+    fun saveSharedPref(resultado : Float){
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()){
+            putFloat(getString(R.string.saved_calc), resultado)
+            apply()
+
+        }
+    }
+
+    fun getSharedPref() : Float{
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        return sharedPref.getFloat(getString(R.string.saved_calc), 0.0f)
     }
 }
